64a69b5f67702e9402a4499e7f52f0cf26ac63ca
dmitriy <dmitry.petrov@mentormate.com>
2022-06-23 13:24:42 +0300

    The /api route can now be used with query parameters; can be used as follows:
    ```
    /api?first=N1&last=N2&timeseries=X&timeseries=B&timeseries=C
    ```
    
    The request above will get all timeseries for the specified interval.
    If any of the `timeseries` properties is ommited, it won't be received.
    `last` and  `first` can be skipped (both or just one of them) in which case `first = 0` and `last = Date.now()`.

--- servers/express.js
+++ servers/express.js
@@ -22,10 +22,21 @@ const argv = require('minimist')(process.argv.slice(2), argvOptions);
 const pathToLogFile = argv['logfile'];
 const app = express();
 
-app.get('/api', function(req, res) {
+app.get("/api", function (req, res) {
+  let first = req.query.first || 0;
+  let last = req.query.last || Date.now();
   let timeseries = {eventloop: eventloopDataRows, memory: memoryDataRows, cpu: cpuDataRows};
-  let data = {version, timeseries};
-  res.status(200).send(data);
+
+  for (const key of Object.keys(timeseries)) {
+    if (!req.query.timeseries.includes(key)) {
+      delete timeseries[key];
+    }
+  };
+
+  for (const [key, value] of Object.entries(timeseries)) {
+    timeseries[key] = timeseries[key].filter(e => (e.ts >= first && e.ts <= last));
+  };
+  return res.status(200).send({version, range: { first, last }, timeseries});
 });
 
 // the datarows
@@ -57,7 +68,7 @@ async function collector() {
 
       if (record.type === 'eventloop') {
         // Eventloop delay is in nanoseconds. Make it ms.
-        const row = {delta};
+        const row = {ts: record.ts, delta: delta};
         const percentiles = [50, 75, 90, 95, 99];
         for (let i = percentiles.length - 1; i >= 0 ; i--) {
           row[percentiles[i]] = entry[percentiles[i]] / 1e6;
@@ -70,8 +81,8 @@ async function collector() {
         const heapTotal = entry['heapTotal'] / 1e6;
         const rss = entry['rss'] / 1e6;
         // Cpu data is in microseconds. Make it ms
-        cpuDataRows.push({delta: delta, user: entry['cpuUserAvg'] / 1e3, system: entry['cpuSystemAvg'] / 1e3});
-        memoryDataRows.push({delta, rss, heapTotal, heapUsedAvg, externalAvg});
+        cpuDataRows.push({ts: record.ts, delta: delta, user: entry['cpuUserAvg'] / 1e3, system: entry['cpuSystemAvg'] / 1e3});
+        memoryDataRows.push({ts: record.ts, delta, rss, heapTotal, heapUsedAvg, externalAvg});
       } else {
         continue;
       };

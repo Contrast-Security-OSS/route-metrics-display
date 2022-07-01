9795f16e9c0f33fc57bf9ea627f39359035aa6ac
Kaloyan Peev <kaloyan.peev@ad.mentormate.bg>
2022-07-01 18:35:08 +0300

    Made small changes for initial data fetching and periodic data fetching

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,22 +3,22 @@ import React, {useEffect} from "react";
 import ChartScreen from "./components/charts/chart-screen";
 import {useDispatch} from "react-redux";
 import {addData} from "./redux/slices/dataSlice";
+import {useFetch} from "./utils/useFetch";
 
 const App = () => {
 	const dispatch = useDispatch();
+	const {error, loading, fetchData} = useFetch();
+
 	useEffect(() => {
-		fetch(
-			`http://localhost:3001/api?last=${Date.now()}&first=${Date.now() -
+		const applyData = (data) => {
+			dispatch(addData(data));
+		};
+
+		fetchData({
+			query: `timeseries=cpu&last=${Date.now()}&first=${Date.now() -
 				20000 * 60}`,
-			{
-				method: "GET",
-			}
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				dispatch(addData(data));
-			})
-			.catch((err) => console.log(err));
+			applyData: applyData,
+		});
 	}, []);
 
 	return (
--- front-end/src/components/charts/chart-screen.jsx
+++ front-end/src/components/charts/chart-screen.jsx
@@ -6,30 +6,32 @@ import {StyledChartScreen} from "./chart-screen.styles";
 import LineChart from "./line-chart";
 import {addData} from "../../redux/slices/dataSlice";
 import {useDispatch} from "react-redux";
+import {useFetch} from "../../utils/useFetch";
 
 const ChartScreen = () => {
 	const chartData = useSelector((state) => state.data.timeseries);
 	const dispatch = useDispatch();
+	const {error, loading, fetchData} = useFetch();
+
 	useEffect(() => {
+		const applyData = (data) => {
+			dispatch(addData(data));
+		};
 		const interval = setInterval(() => {
-			fetch(
-				`http://localhost:3001/api?last=${Date.now()}&first=${Date.now() -
+			fetchData({
+				query: `timeseries=cpu&last=${Date.now()}&first=${Date.now() -
 					20000 * 60}`,
-				{
-					method: "GET",
-				}
-			)
-				.then((res) => res.json())
-				.then((data) => {
-					dispatch(addData(data));
-				})
-				.catch((err) => console.log(err));
+				applyData: applyData,
+			});
 		}, 10000);
 		return () => {
 			clearInterval(interval);
 		};
 	}, [chartData]);
 
+	if (error) {
+		return <h1>{error.toString()}</h1>;
+	}
 	if (chartData) {
 		if (Object.keys(chartData).length !== 0) {
 			const charts = Object.entries(chartData).map((chart) => {

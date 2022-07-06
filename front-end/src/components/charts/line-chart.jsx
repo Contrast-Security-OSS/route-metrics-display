38859394a54ec73fc80c8fa9eeea20d539b03a42
Kaloyan Peev <kaloyan.peev@ad.mentormate.bg>
2022-07-06 14:08:27 +0300

    Added switch statement

--- front-end/src/components/charts/line-chart.jsx
+++ front-end/src/components/charts/line-chart.jsx
@@ -7,67 +7,74 @@ import SaveButton from "../buttons/save-button";
 const LineChart = ({chartTitle, chartData}) => {
   let datasets = [];
   let labels = chartData.map((dataObj) => Math.floor(dataObj.delta));
-  if (chartTitle === "cpu") {
-    const userData = chartData.map((dataObj) => dataObj.user);
-    const systemData = chartData.map((dataObj) => dataObj.system);
-    datasets.push(
-      {
-        label: `user`,
-        data: userData,
-        backgroundColor: ["#BAD4C6ff"],
-        borderColor: ["#2C4244ff", "#2D8840ff;"],
-        borderWidth: 1,
-      },
-      {
-        label: `system`,
-        data: systemData,
-        backgroundColor: ["#72524a"],
-        borderColor: ["#ebcfc9", "#2D8840ff;"],
-        borderWidth: 1,
-      }
-    );
-  } else if (chartTitle === "eventloop") {
-    const eventloopLabels = Object.keys(chartData[0]).filter((key) => {
-      if (!isNaN(key)) {
-        return key;
-      }
-    });
-    const eventloopDatasets = eventloopLabels.map((label) =>
-      chartData.map((obj) => obj[label])
-    );
-    datasets = eventloopLabels.map((label, index) => {
-      //Random colors are a temporary solution
-      let randomColor;
-      if (randomColor !== null) {
-        randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      }
-      return {
-        label: label,
-        data: eventloopDatasets[index],
-        backgroundColor: ["#" + randomColor],
-        borderColor: ["#" + randomColor, "#" + randomColor],
-        borderWidth: 1,
-      };
-    });
-  } else if (chartTitle === "memory") {
-    const firstDataSet = chartData.map((dataObj) => dataObj["externalAvg"]);
-    const secondDataSet = chartData.map((dataObj) => dataObj["heapUsedAvg"]);
-    datasets.push(
-      {
-        label: `externalAvg`,
-        data: firstDataSet,
-        backgroundColor: ["#BAD4C6ff"],
-        borderColor: ["#2C4244ff", "#2D8840ff;"],
-        borderWidth: 1,
-      },
-      {
-        label: `heapUsedAvg`,
-        data: secondDataSet,
-        backgroundColor: ["#72524a"],
-        borderColor: ["#ebcfc9", "#2D8840ff;"],
-        borderWidth: 1,
-      }
-    );
+
+  switch (chartTitle) {
+    case "cpu":
+      const userData = chartData.map((dataObj) => dataObj.user);
+      const systemData = chartData.map((dataObj) => dataObj.system);
+      datasets.push(
+        {
+          label: `user`,
+          data: userData,
+          backgroundColor: ["#BAD4C6ff"],
+          borderColor: ["#2C4244ff", "#2D8840ff;"],
+          borderWidth: 1,
+        },
+        {
+          label: `system`,
+          data: systemData,
+          backgroundColor: ["#72524a"],
+          borderColor: ["#ebcfc9", "#2D8840ff;"],
+          borderWidth: 1,
+        }
+      );
+      break;
+    case "eventloop":
+      const eventloopLabels = Object.keys(chartData[0]).filter((key) => {
+        if (!isNaN(key)) {
+          return key;
+        }
+      });
+      const eventloopDatasets = eventloopLabels.map((label) =>
+        chartData.map((obj) => obj[label])
+      );
+      datasets = eventloopLabels.map((label, index) => {
+        //Random colors are a temporary solution
+        let randomColor;
+        if (randomColor !== null) {
+          randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        }
+        return {
+          label: label,
+          data: eventloopDatasets[index],
+          backgroundColor: ["#" + randomColor],
+          borderColor: ["#" + randomColor, "#" + randomColor],
+          borderWidth: 1,
+        };
+      });
+      break;
+    case "memory":
+      const firstDataSet = chartData.map((dataObj) => dataObj["externalAvg"]);
+      const secondDataSet = chartData.map((dataObj) => dataObj["heapUsedAvg"]);
+      datasets.push(
+        {
+          label: `externalAvg`,
+          data: firstDataSet,
+          backgroundColor: ["#BAD4C6ff"],
+          borderColor: ["#2C4244ff", "#2D8840ff;"],
+          borderWidth: 1,
+        },
+        {
+          label: `heapUsedAvg`,
+          data: secondDataSet,
+          backgroundColor: ["#72524a"],
+          borderColor: ["#ebcfc9", "#2D8840ff;"],
+          borderWidth: 1,
+        }
+      );
+      break;
+    default:
+      break;
   }
 
   return (
